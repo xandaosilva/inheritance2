d7bd54844a05ea299708e96913f842a6961afd10
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-21 10:53:25 -0300

    Develop Employee and OutsourcedEmployee classess

--- Inheritance2/Entities/Employee.cs
+++ Inheritance2/Entities/Employee.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Inheritance2.Entities
 {
     class Employee
     {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Name + " - $" + Payment().ToString("F2",CI));
+            return sb.ToString();
+        }
     }
 }
--- Inheritance2/Entities/OutsourcedEmployee.cs
+++ Inheritance2/Entities/OutsourcedEmployee.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Inheritance2.Entities
 {
-    class OutsourcedEmployee
+    class OutsourcedEmployee : Employee
     {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name,hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
     }
 }
